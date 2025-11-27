d691cafa72c6a9c804c276ff95c8688a64113326
Adelin-GM <arelin650@gmail.com>
2025-11-27 19:19:42 +0000

    apply theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+    import './layout.css';
+</script>
+
 <div class="scene">
 	<slot />
 </div>
@@ -11,6 +15,6 @@
 		height: 100%;
 		width: 100%;
 		position: relative;
-		background: radial-gradient(hsl(220 14% 20%), hsl(220 20% 10%));
+		/* background: radial-gradient(hsl(220 14% 20%), hsl(220 20% 10%)); */
 	}
 </style>
