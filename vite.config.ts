dc297e1e2c1e906489cb1b71833b31e09304934b
Adelin-GM <arelin650@gmail.com>
2025-11-27 20:00:35 +0000

    add adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-vercel';
+
+// export default defineConfig({
+// 	plugins: [tailwindcss(), sveltekit()]
+// });
+
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  kit: {
+    adapter: adapter(),
+    // Other kit options go here
+  },
+  define: {
+    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
+  },
+  plugins: [tailwindcss(), sveltekit()]
 });
