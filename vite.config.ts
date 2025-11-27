c4c53c5bbadcf8194cd3269141eb01c219268876
Adelin-GM <arelin650@gmail.com>
2025-11-27 20:04:14 +0000

    remove kit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import adapter from '@sveltejs/adapter-vercel';
-
-// export default defineConfig({
-// 	plugins: [tailwindcss(), sveltekit()]
-// });
-
 
 export default defineConfig({
-  kit: {
-    adapter: adapter(),
-    // Other kit options go here
-  },
   define: {
     'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
   },
